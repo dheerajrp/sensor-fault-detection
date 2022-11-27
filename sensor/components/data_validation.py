8198c366d4b0dad2a311a4ff414a81edc406ca73
Dheeraj RP <dheerajrp66@gmail.com>
2022-11-27 14:29:10 +0530

    added data validation

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -1,4 +1,7 @@
+import os
+
 import pandas
+from scipy.stats import ks_2samp
 
 from sensor.constants.training_pipeline import SCHEMA_FILE_PATH
 from sensor.entity.artifact_entity import DataValidationArtifact, \
@@ -6,7 +9,7 @@ from sensor.entity.artifact_entity import DataValidationArtifact, \
 from sensor.entity.config_entity import DataValidationConfig
 from sensor.exceptions import SensorException
 from sensor.logger import logging
-from sensor.utils.main_utils import read_yaml_file
+from sensor.utils.main_utils import read_yaml_file, write_yaml_file
 
 
 class DataValidation:
@@ -21,7 +24,7 @@ class DataValidation:
 
     def validate_number_of_columns(self, dataframe: pandas.DataFrame) -> bool:
         try:
-            number_of_columns = self._schema_config['columns']
+            number_of_columns = len(self._schema_config['columns'])
             if number_of_columns == len(dataframe.columns):
                 return True
             return False
@@ -52,9 +55,30 @@ class DataValidation:
         except Exception as error:
             raise SensorException(error)
 
-    def detect_data_drift(self):
+    def detect_data_drift(self, base_df, current_df, threshold=0.05) -> bool:
         try:
-            pass
+            report = {}
+            status = True
+            for column in base_df.columns:
+                d1 = base_df[column]
+                d2 = current_df[column]
+                ks_out = ks_2samp(d1, d2)
+                if ks_out.pvalue >= threshold:
+                    is_drift = False
+                else:
+                    is_drift = True
+                    status = False
+                report.update({column: {'p_value': float(ks_out.pvalue),
+                                        'drift_status': is_drift}})
+            drift_report_file_path = \
+                self.data_validation_config.drift_report_file_path
+
+            # creating the directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path, content=report)
+            return status
+
         except Exception as error:
             raise SensorException(error)
 
@@ -92,6 +116,22 @@ class DataValidation:
             if len(error_message) > 0:
                 raise Exception(error_message)
 
-
+            # check for data drift
+            status = self.detect_data_drift(base_df=train_dataframe,
+                                            current_df=test_dataframe)
+            data_validation_artifact = DataValidationArtifact(
+                validation_status=status,
+                valid_train_file_path=self.data_ingestion_artifact
+                .train_file_path,
+                valid_test_file_path=self.data_ingestion_artifact
+                .test_file_path,
+                invalid_train_file_path=self.data_validation_config
+                .invalid_train_file_path,
+                invalid_test_file_path=self.data_validation_config
+                .invalid_test_file_path,
+                drift_report_file_path=self.data_validation_config
+                .drift_report_file_path
+            )
+            return data_validation_artifact
         except Exception as error:
             raise SensorException(error)
--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -1,9 +1,12 @@
 import sys
 
 from sensor.components.data_ingestion import DataIngestion
-from sensor.entity.artifact_entity import DataIngestionArtifact
+from sensor.components.data_validation import DataValidation
+from sensor.entity.artifact_entity import DataIngestionArtifact, \
+    DataValidationArtifact
 from sensor.entity.config_entity import (TrainingPipelineConfig,
-                                         DataIngestionConfig)
+                                         DataIngestionConfig,
+                                         DataValidationConfig)
 from sensor.exceptions import SensorException
 from sensor.logger import logging
 
@@ -26,9 +29,19 @@ class TrainPipeline:
         except Exception as error:
             raise SensorException(error, sys)
 
-    def start_data_validation(self):
+    def start_data_validation(self,
+                              data_ingestion_artifact: DataIngestionArtifact
+                              ) -> DataValidationArtifact:
         try:
-            pass
+            data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=data_validation_config)
+            data_validation_artifact \
+                = data_validation.initiate_data_validation()
+            logging.info(f"Completed data validation. . Artifact: "
+                         f"{data_validation_artifact}")
+            return data_validation_artifact
         except Exception as error:
             raise SensorException(error, sys)
 
@@ -59,5 +72,6 @@ class TrainPipeline:
     def run_pipeline(self):
         try:
             data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
+            data_validation_artifact: DataValidationArtifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
         except Exception as error:
             raise SensorException(error, sys)
